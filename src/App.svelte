a1232c1663c588122040a40c41e4edf1432917ae
Alexandro Disla <alexandrodisla@hotmail.com>
2020-09-24 17:23:53 -0400

    firebase and svelte

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { FirebaseApp, User } from "sveltefire";
+  import firebase, { auth } from "firebase/app";
+  import "firebase/firestore";
+  import "firebase/auth";
+  import AuthForm from "./components/AuthForm.svelte";
+  const firebaseConfig = {
+    apiKey: "AIzaSyBEEBzUQlTN7DyvlBt6Y6XU4IU4L503oy0",
+    authDomain: "svelte-chat-487fe.firebaseapp.com",
+    databaseURL: "https://svelte-chat-487fe.firebaseio.com",
+    projectId: "svelte-chat-487fe",
+    storageBucket: "svelte-chat-487fe.appspot.com",
+    messagingSenderId: "553941000601",
+    appId: "1:553941000601:web:138c5cdae4eb02731df308",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
+
+<main>
+  <FirebaseApp {firebase}>
+    <User let:user let:auth>
+      <div slot="signed-out">
+        <AuthForm {auth} />
+      </div>
+    </User>
+  </FirebaseApp>
+</main>
